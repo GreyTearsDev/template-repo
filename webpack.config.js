fe284c35accbe0d2d3535ccf7f6b86fc22bc7b0f
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2023-12-22 18:54:03 +0100

    Get the template ready

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  // entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: "",
+      metaDesc: "GitHub @GreyTearsDev",
+      template: "src/index.html",
+      inject: "body",
+    }),
+  ],
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+};
