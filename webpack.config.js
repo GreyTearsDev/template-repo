20d336afe0bd2c63292f33b00122c9ff78bc121c
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-17 14:10:34 +0100

    initial config

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,33 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const mode =
+  process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
 module.exports = {
-  // entry: "./src/index.js",
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+  mode: mode,
   module: {
     rules: [
       {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader'],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader'],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(?:js|mjs|cjs)$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: [['@babel/preset-env', { targets: 'defaults' }]],
-          },
         },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
     ],
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      hash: true,
-      title: '',
-      metaDesc: 'GitHub @GreyTearsDev',
-      template: 'src/index.html',
-      inject: 'body',
-    }),
-  ],
-  devtool: 'inline-source-map',
+
+  devtool: 'source-map',
   devServer: {
     static: './dist',
   },
